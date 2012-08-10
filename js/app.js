4db8adeb8d94cb3d331ac2ceefcd34e23a9b66be
Peter Hart <cinphart@gmail.com>
2012-08-09 23:40:43 -0400

    Introduce an initial directive in app.js, which allows me to update the fillcolor attribute (which appears to actually be a property on the dom element).
    The plan is to extend this further, first to implement some simple
    structures for VML with a few attributes/properties that can be
    interpolated, and then to extend this to SVG as well, with the
    directive being automatically set up 'correctly' for the environment
    on startup. Then to try to pull in more and more SVG/VML elements as
    appropriate, to provide a Raphael-style layer over the common
    denominators of both, fit as much as possible into angular directives
    as an idiom.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
 var doc = window.document;
-// set up IE to handle VML
-doc.createStyleSheet().addRule('.rvml','behavior:url(#default#VML)');
-doc.getElementById('output').innerText = "Stylesheet Created";
-doc.namespaces.add("rvml","urn:schemas-microsoft-com:vml");
-doc.getElementById('output').innerText = "Namespace Created";
 
-// Create an oval
-var oval = doc.createElement("<rvml:oval fillcolor='green' style='position:absolute;top:0;left:0;width:50;height:50' class='rvml'/>");
-// Create a rectangle
-var rect = doc.createElement("<rvml:rect fillcolor='red' style='position:absolute;top:52;left:0;width:50;height:50' class='rvml'/>");
+var type = (window.SVGAngle || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1"))?"SVG":"VML";
+
+var op = document.getElementById('output');
+
+function log(x) {
+    window.console && console.log(x);
+    op.removeChild(op.firstChild);
+    op.appendChild(document.createTextNode(x));
+}
+
+if (type == "VML") {
+    
+    // set up IE to handle VML
+    doc.createStyleSheet().addRule('.rvml','behavior:url(#default#VML)');
+    log("Stylesheet Created");
+    doc.namespaces.add("rvml","urn:schemas-microsoft-com:vml");
+    log("Namespace Created");
+
+    // Create an oval
+    //var oval = doc.createElement("<rvml:oval fillcolor='green' style='position:absolute;top:0;left:0;width:50;height:50' class='rvml'/>");
+    // Create a rectangle
+    //var rect = doc.createElement("<rvml:rect fillcolor='red' style='position:absolute;top:52;left:0;width:50;height:50' class='rvml'/>");
 
 
-// Add them both to the document
-doc.getElementById('output').innerText = "Element Created";
-doc.getElementById('canvas').appendChild(oval);
-doc.getElementById('canvas').appendChild(rect);
-doc.getElementById('output').innerText = "Element Appended";
+    // Add them both to the document
+    log("Elements Created");
+    //doc.getElementById('canvas').appendChild(oval);
+    //doc.getElementById('canvas').appendChild(rect);
+    log("Element Appended");
+}
+
+log("Implementation is: " + type + ", canvas style is: " + document.getElementById('canvas').style.cssText);
+
+angular.module('myApp',[])
+   .directive(
+       'vcFillColor',
+       function(){
+           log("directive set up");
+           return function postLink(scope, elem, attrs, controller) {
+                   log(elem[0]);
+                   log(attrs.vcFillColor);
+                   attrs.$observe('vcFillColor',
+                                   function(value){
+                                       log("vcFillColor Observed change to: " + value);
+                                       elem[0].fillcolor = value;
+                                   });
+               };
+       });
+
